a1641ef8f119eff9166629124193bba288b90f78
Annomaliiya <ivasyukanna@gmail.com>
2021-11-24 17:53:51 +0200

    Updates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,108 @@
 'use strict';
 
+//импорты
 import './css/main.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// import SimpleLightbox from "simplelightbox";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchImages } from './js/pixabay-api';
+import galleryCardsTemplate from './templates/gallery-card.hbs';
+///////////////////////////////
 
+// вытаскиваем елементы из html
+const searchFormEl = document.querySelector('#search-form');
+const galleryEl = document.querySelector('.gallery');
+const btnSearch = document.querySelector('.btn-submit');
+const loadMoreBtn = document.querySelector('.load-more');
+///////////////////////////////
+
+// вспомагательные функции для поиска
+const clearGallery = () => {
+    galleryEl.innerHTML = '';
+};
+
+const showLoadMoreBtn = () => {
+    loadMoreBtn.classList.remove('is-hidden');
+};
+const increasePageInLocalStorage = () => {
+    const pageIterattor = Number(localStorage.getItem('page')) + 1;
+    localStorage.setItem('page', JSON.stringify(pageIterattor));
+};
+// 
+
+// ПОИСК ИЗОБРАЖЕНИЙ ЧЕРЕЗ ФОРМУ
+const onFormSubmit = event => {
+    event.preventDefault();
+
+    localStorage.setItem('page', '0');
+
+    const query = document.querySelector('.input-box').value.trim();
+    localStorage.setItem('query', query);
+    clearGallery();
+
+    fetchImages(query)
+        .then(({ data }) => {
+            console.log(data);
+            const { hits, totalHits } = data;
+            console.log(hits)
+
+            galleryEl.insertAdjacentHTML('beforeend', galleryCardsTemplate(hits, totalHits));
+            const lightbox = new SimpleLightbox('.gallery a', {
+                captionDelay: 250
+            });
+            showLoadMoreBtn();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+// кнопка загрузить больше
+
+
+const onLoadMoreBtnClick = () => {
+    const query = localStorage.getItem('query');
+
+    fetchImages(query,)
+        .then(({ data }) => {
+            const { hits, totalHits } = data;
+            galleryEl.insertAdjacentHTML('beforeend', galleryCardsTemplate(hits, totalHits));
+            increasePageInLocalStorage();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+
+// вешаем слушателей событий
+
+searchFormEl.addEventListener('submit', onFormSubmit);
+loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
+
+// подключаем simplylightbox
+
+// const lightbox = new SimpleLightbox('.gallery a', {
+//     captionDelay: 250
+// });
+
+
+// let mar = imgCardTmp(hits);
+// function imgCardTmp(hits) {
+//     return images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+//         return `<div class="photo-card">
+//         <a href="${largeImageURL}"
+//                 <img src="${webformatURL}"
+//                      lt="${tags}"
+//                      data-src="${largeImageURL}"
+//                      width="270"
+//                      height="auto"
+//                      loading="lazy" />
+//                 <div class="info">
+//                   <p class="info-item"><b>Likes:</b>${likes}</p>
+//                   <p class="info-item"><b>Views:</b>${views}</p>
+//                   <p class="info-item"><b>Comments:</b>${comments}</p>
+//                   <p class="info-item"><b>Downloads:</b>${downloads}</p>
+//                 </div>
+//               </div>`
+//     }).join("")
+// }
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,15 +1,33 @@
+import axios from 'axios';
+
+
 const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = "24480892-2cf9ff0ac9dbac3af2a958edd";
 
 export const fetchImages = query => {
+    let page = Number(localStorage.getItem('page'));
+    page += 1;
+    return axios.get(`${BASE_URL}`, {
+        params: {
+            key: API_KEY,
+            q: query,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+            page: page,
+            per_page: 40,
+        },
+    })
 
-    return fetch(
-        `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`,
-    ).then(response => {
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
+    // }?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`);
 
-        return response.json();
-    });
+    // return fetch(
+    //     `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`,
+    // ).then(response => {
+    //     if (!response.ok) {
+    //         throw new Error(response.status);
+    //     }
+
+    //     return response.json();
+    // });
 };
